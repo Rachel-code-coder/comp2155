d334dc5af1ede98dddefc41241d91fbd433bd406
Rachel-code-coder <vy.ngothuyy@gmail.com>
2025-09-10 08:41:31 -0400

    4

---
+++ wk1/main.py
@@ -0,0 +1,34 @@
+class Grade:
+    def __init__(self, percentage):
+        self.percentage = percentage
+
+    @property
+    def percentage(self):
+        return self.__percentage
+
+
+    @percentage.setter
+    def percentage(self, value:int)->None:
+        if isinstance(value, int) and value >= 0 and value <= 100:
+            self.__percentage = value
+        else:
+            raise ValueError("Invalid percentage")
+
+class PassingGrade(Grade):
+    def __init__(self, percentage):
+        super().__init__(percentage)
+
+    @property
+    def percentage(self):
+        return self.__percentage
+
+    @percentage.setter
+    def percentage(self, value):
+
+        if isinstance(value, int) and value < 50:
+            raise ValueError("This is not a passing grade")
+
+my_grade = Grade(100)
+print(my_grade.percentage)
+
+pg = PassingGrade(50)
